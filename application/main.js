0d7cb5f2b62ad5f5312ac74ac026e94e23dbbf97
cameronblack <camblack0567@icloud.com>
2024-05-12 21:04:01 -0700

    feat: add gh workflows and electron base

---
+++ application/main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow, screen } = require('electron');
+const path = require('path');
+
+function createWindow() {
+    app.whenReady().then(() => {
+        const primaryDisplay = screen.getPrimaryDisplay();
+        const { width, height } = primaryDisplay.workAreaSize;
+        const mainWindow = new BrowserWindow({
+            width: width / 2,
+            height: height / 2,
+            webPreferences: {
+                //preload: path.join(__dirname, 'preload.js'),
+                nodeIntegration: false, // for security reasons
+                contextIsolation: true,  // for security reasons
+                enableRemoteModule: false // for security reasons
+            },
+            resizable: true
+        });
+
+        mainWindow.loadFile(path.join(__dirname, 'web/index.html'));
+        if (process.env.NODE_ENV === 'development')
+            mainWindow.webContents.openDevTools();
+    });
+}
+app.whenReady().then(createWindow);
+
+app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit();
+});
