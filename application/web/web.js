d46bac1f26459e7d618d64179ba2bc0c63ccf52b
niquach <niquach@ucsd.edu>
2024-05-21 10:41:55 -0700

    added js functionality to the sidebar collapse button

--- application/web/web.js
+++ application/web/web.js
@@ -1,3 +1,6 @@
 window.onload = () => {
-    
+    document.getElementById('collapse-button').addEventListener('click', function(event) {
+        const sidebar = document.querySelector('sidebar');
+        sidebar.classList.toggle('sidebar-collapsed');
+    });
 };
