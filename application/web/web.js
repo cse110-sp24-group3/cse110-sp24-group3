26ad6fbf56e9fd39a935ca602e956f450b506db8
Benjamin Nhan <benjaminnhan12@gmail.com>
2024-05-20 20:57:19 -0700

    Added a scroll and hover feature for the 3 dots

--- application/web/web.js
+++ application/web/web.js
@@ -1,3 +1,22 @@
 window.onload = () => {
-    
+    createSidebar();
+};
+
+function createSidebar() {
+
+     const journalEntries = document.querySelectorAll('.journal-entry');
+
+     journalEntries.forEach(entry => {
+
+        const faIcon = entry.querySelector('.fa.fa-ellipsis-v');
+
+        entry.addEventListener('mouseover', () => {
+            faIcon.style.display = 'block';
+        });
+
+        entry.addEventListener('mouseout', () => {
+            faIcon.style.display = 'none';
+        });
+        
+     });
 };
