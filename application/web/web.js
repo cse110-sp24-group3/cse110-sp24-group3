08a62186c9c4145f8b89ec8e8a39ee39561677a7
Benjamin Nhan <benjaminnhan12@gmail.com>
2024-05-22 02:08:37 -0700

    feat: small edit with pulled code

--- application/web/web.js
+++ application/web/web.js
@@ -1,5 +1,18 @@
 window.onload = () => {
     createSidebar();
+    
+    document.getElementById('collapse-button').addEventListener('click', toggleSidebar);
+
+    function toggleSidebar() {
+    const sidebar = document.querySelector('sidebar');
+    sidebar.classList.toggle('sidebar-collapsed');
+    
+    const newJournalBtn = document.querySelector('.new-journal');
+    newJournalBtn.classList.toggle('toggled-new-journal');
+    
+    const journalEntryBtn = document.querySelector('.journal-entry');
+    journalEntryBtn.classList.toggle('toggled-journal-entry');
+};
 };
 
 
@@ -25,16 +38,4 @@ function createSidebar() {
         }
     });
 
-};
-document.getElementById('collapse-button').addEventListener('click', toggleSidebar);
-
-function toggleSidebar() {
-    const sidebar = document.querySelector('sidebar');
-    sidebar.classList.toggle('sidebar-collapsed');
-    
-    const newJournalBtn = document.querySelector('.new-journal');
-    newJournalBtn.classList.toggle('toggled-new-journal');
-    
-    const journalEntryBtn = document.querySelector('.journal-entry');
-    journalEntryBtn.classList.toggle('toggled-journal-entry');
 };
