aa6e3a018ff653751da0c04b0555d7873bde4906
Azpi <jesusazpitartedoesart@gmail.com>
2024-05-21 21:19:10 -0700

    fix: fixed lint issue

--- application/web/assets/scripts/entry-title-script.js
+++ application/web/assets/scripts/entry-title-script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', (event) => {
+document.addEventListener('DOMContentLoaded', () => {
     const titleInput = document.getElementById('title-input');
 
     titleInput.addEventListener('focus', () => {
