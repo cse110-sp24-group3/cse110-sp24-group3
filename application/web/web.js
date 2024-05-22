07e0f193c4be19c98e65bfca4454100a4fcc82c6
Nicholas Quach <nquach1515@gmail.com>
2024-05-21 17:56:37 -0700

    [feat] removed journal buttons when sidebar is collapsed

--- application/web/web.js
+++ application/web/web.js
@@ -1,6 +1,17 @@
 window.onload = () => {
-    document.getElementById('collapse-button').addEventListener('click', function(event) {
+    document.getElementById('collapse-button').addEventListener('click', toggleSidebar);
+
+    function toggleSidebar() {
         const sidebar = document.querySelector('sidebar');
         sidebar.classList.toggle('sidebar-collapsed');
-    });
+
+        const sidebarModule = document.querySelector('.sidebar-module');
+        sidebarModule.classList.toggle('toggled-sidebar-module')
+
+        const newJournalBtn = document.querySelector('.new-journal');
+        newJournalBtn.classList.toggle('toggled-new-journal');
+        
+        const journalEntryBtn = document.querySelector('.journal-entry');
+        journalEntryBtn.classList.toggle('toggled-journal-entry');
+    }
 };
