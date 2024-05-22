b5ad3bd078657c9dfe55d67762ff9d1c8e56fce2
sjting8 <122575130+sjting8@users.noreply.github.com>
2024-05-21 23:03:12 -0700

    refactor: moved script to web.js and added proper margins to space out journals on the sidebar

--- application/web/web.js
+++ application/web/web.js
@@ -1,3 +1,11 @@
 window.onload = () => {
     
 };
+
+function newJournal() {
+    console.log('hi');
+    var container = document.getElementById("sidebar-module");
+    var journal = document.getElementById("journal-entries");
+    var newJournal = journal.cloneNode(true);
+    container.appendChild(newJournal);
+}
