ad8bb00cb6eeb0eec13253d097e93a692d89e3f4
reisandylamdjani <lamdjani@gmail.com>
2024-05-21 18:01:33 -0700

    feat: added discard popup window for cancel changes

--- application/web/web.js
+++ application/web/web.js
@@ -1,3 +1,23 @@
 window.onload = () => {
-    
+    showDiscardChangePage();
 };
+
+function showDiscardChangePage() {
+    const button = document.getElementById("myButton");
+    const myPopup = document.getElementById("myPopup");
+    
+    button.addEventListener("click", () => {
+            myPopup.classList.add("show");
+        }
+    );
+    closePopup.addEventListener("click", () => {
+            myPopup.classList.remove("show");
+        }
+    );
+    window.addEventListener("click", (event) => {
+            if (event.target == myPopup) {
+                myPopup.classList.remove("show");
+            }
+        }
+    );
+}
