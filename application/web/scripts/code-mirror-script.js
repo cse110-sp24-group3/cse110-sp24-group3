fb1611d1910922d8429955bac12cb58049b95ad7
Corrente <logan6656@gmail.com>
2024-05-29 22:20:39 -0700

    fix

--- application/web/scripts/code-mirror-script.js
+++ application/web/scripts/code-mirror-script.js
@@ -6,9 +6,13 @@
 //TODO: 
 // * Full programming language support
 // * Live rendering 
+// * Modify styling
 
 document.addEventListener("DOMContentLoaded", function() {
+    
     var textArea = document.getElementById("entry-textarea")
+    
+    // Create the CodeMirror Object
     var editor = CodeMirror(textArea, {
         mode: "gfm",
         lineNumbers: true,
@@ -18,7 +22,13 @@ document.addEventListener("DOMContentLoaded", function() {
         highlightingFormat: true,
         fencedCodeBlocks: true,
         fencedCodeBlockHighlighting: true,
-
     });
+    
+    // Not sure why, but the formatting messes up without this
     editor.refresh();
+
+    // Style the editor
+    // I'm sure this could be moved to main.css 
+    // But its here for now
+    editor.style['border-radius'] = '10px';
 });
