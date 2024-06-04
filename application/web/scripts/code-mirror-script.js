f0993f202ec3854a96fc75dd47dac533119cddf6
Corrente <71531248+C-rrente@users.noreply.github.com>
2024-06-03 17:15:54 -0700

    Code mirror (#102)
    * [feat] Added codemirror entry, very basic
    
    * [feat] partial code highlight support
    
    * fix
    
    * fix merged branch
    
    * fix: added dependency
    
    * fix: added dependencies
    
    * fix: attempt
    
    * fix: reversed code
    
    * fix: removed java include
    
    * fix: adjusted the codemirror box
    
    * fix: added lint comment
    
    * feat: removed CDN now local
    
    ---------
    
    Co-authored-by: cameronblack <camblack0567@icloud.com>

---
+++ application/web/scripts/code-mirror-script.js
@@ -0,0 +1,29 @@
+// LRC
+//////////////////////////////////////////////////////////////////////////////////
+///* This is where the CodeMirror editor is applied to the entry-textarea div *///
+//////////////////////////////////////////////////////////////////////////////////
+
+//TODO: 
+// * Full programming language support
+// * Live rendering 
+// * Modify styling
+
+document.addEventListener("DOMContentLoaded", function() {
+    
+    let textArea = document.getElementById("entry-textarea")
+    
+    // Create the CodeMirror Object
+    let editor = CodeMirror(textArea, {// eslint-disable-line
+        mode: "gfm",
+        lineNumbers: true,
+        lineWrapping: true,
+        styleActiveLine: true,
+        matchBrackets: true,
+        highlightingFormat: true,
+        fencedCodeBlocks: true,
+        fencedCodeBlockHighlighting: true,
+    });
+    
+    // Not sure why, but the formatting messes up without this
+    editor.refresh();
+});
