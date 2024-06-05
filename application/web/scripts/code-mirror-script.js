3d0d826f2205f961aef8ea8e942dbc1e4becb9f3
C-rrente <logan6656@gmail.com>
2024-06-05 12:10:40 -0700

    feat: attempting to add live-rendering

--- application/web/scripts/code-mirror-script.js
+++ application/web/scripts/code-mirror-script.js
@@ -9,21 +9,60 @@
 // * Modify styling
 
 document.addEventListener("DOMContentLoaded", function() {
-    
-    let textArea = document.getElementById("entry-textarea")
-    
-    // Create the CodeMirror Object
-    let editor = CodeMirror(textArea, {// eslint-disable-line
-        mode: "gfm",
-        lineNumbers: true,
-        lineWrapping: true,
-        styleActiveLine: true,
-        matchBrackets: true,
-        highlightingFormat: true,
-        fencedCodeBlocks: true,
-        fencedCodeBlockHighlighting: true,
-    });
-    
-    // Not sure why, but the formatting messes up without this
-    editor.refresh();
+
+  let textArea = document.getElementById("entry-textarea")
+
+
+  const md = window.markdownit();
+
+   //TEST
+
+CodeMirror.defineMode('markdown-preview', function (config) {
+  const markdownMode = CodeMirror.getMode(config, 'gfm');
+  const markdownOverlay = {
+    token: function (stream, state) { // Added the 'state' parameter here
+      const token = markdownMode.token(stream, state.base);
+      if (token.mode) {
+        stream.pos += token.value.length;
+        return null; // Skip the Markdown syntax
+      }
+      return token.style ? token.style + ' ' + token.type : token.type;
+    },
+    blankLine: function (state) {
+      markdownMode.blankLine(state.base);
+    },
+    startState: function () {
+      return {
+        base: CodeMirror.startState(markdownMode)
+      };
+    },
+    copyState: function (state) {
+      return {
+        base: CodeMirror.copyState(markdownMode, state.base)
+      };
+    }
+  };
+  return CodeMirror.multiplexingMode(markdownMode, markdownOverlay);
+});
+
+  // Create the CodeMirror Object
+  let editor = CodeMirror(textArea, {// eslint-disable-line
+    mode: {
+      name: "gfm",
+      overlay: {
+        name: 'markdown-preview',
+        combine: true
+      }
+    },
+    lineNumbers: true,
+    lineWrapping: true,
+    styleActiveLine: true,
+    matchBrackets: true,
+    highlightingFormat: true,
+    fencedCodeBlocks: true,
+    fencedCodeBlockHighlighting: true,
+  });
+
+  // Not sure why, but the formatting messes up without this
+  editor.refresh();
 });
